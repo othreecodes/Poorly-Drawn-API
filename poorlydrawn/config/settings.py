97ca088974d58cd2320562f3bf8fb0e78a169841
Obi Uchenna David <daviduchenna@outlook.com>
2017-11-12 14:21:54 +0100

    readme and code refractor

--- poorlydrawn/config/settings.py
+++ poorlydrawn/config/settings.py
@@ -11,10 +11,11 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+# import environ
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+# BASE_DIR = environ.Path(__file__) 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/

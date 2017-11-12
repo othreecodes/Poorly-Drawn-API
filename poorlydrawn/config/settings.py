b56727531e27d4d563f1e9668822f2ecc72497d9
Obi Uchenna David <daviduchenna@outlook.com>
2017-11-12 16:03:42 +0100

    fixed static files wahalla

--- poorlydrawn/config/settings.py
+++ poorlydrawn/config/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 
-STATIC_ROOT = str(ROOT_DIR('staticfiles'))
+STATIC_ROOT = str(ROOT_DIR('static'))
 
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#static-url
 STATIC_URL = '/static/'

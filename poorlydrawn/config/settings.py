9d95ca70d9e5514403ed4c191c78ec4cac3fe03d
Obi Uchenna David <daviduchenna@outlook.com>
2017-11-12 20:27:51 +0100

    added limit offset implementation to the api

--- poorlydrawn/config/settings.py
+++ poorlydrawn/config/settings.py
@@ -131,7 +131,9 @@ USE_L10N = True
 
 USE_TZ = True
 
-
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination'
+}
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/

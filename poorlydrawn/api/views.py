457574e93d3b567f504f2295b146af6293168ffc
Obi Uchenna David <daviduchenna@outlook.com>
2017-11-12 22:57:25 +0100

    http method names edit

--- poorlydrawn/api/views.py
+++ poorlydrawn/api/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+
 from . import serializers
 from . import models
 from rest_framework.pagination import PageNumberPagination
@@ -12,7 +14,8 @@ from rest_framework.pagination import PageNumberPagination
 class ComicViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.ComicSerializer
     queryset = models.Comic.objects.all()
-
+    permission_classes = [IsAdminUser]
+    http_method_names = ['get']
     # lookup_field = 'slug' #TODO: is slug a better thing to use?
 
     def get_view_description(self, html=False):
